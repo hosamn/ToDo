09bf62f3a5c32d1bf41aec5479d59bb823691fe0
Hosam <34396668+hosamn@users.noreply.github.com>
2023-12-16 00:16:18 +0200

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,3 +1,6 @@
+import os
+import sys
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication, QMessageBox
 
 
@@ -8,18 +11,21 @@ def alert(text):
     msg.setWindowTitle('Alert')
     msg.setText(text)
 
+# Absolute path
+def abspath(f):
+    return os.path.abspath(os.path.dirname(__file__)) + '/' + f
 
 class Gui:
     def __init__(self):
         # self.actions = GuiBehavior(self)
         app = QApplication(sys.argv)
-        app.setWindowIcon(QIcon(abs('ico.ico')))
+        app.setWindowIcon(QIcon(abspath('ico.ico')))
         app.setStyle('Fusion')
-        app.aboutToQuit.connect(self.actions.handle_exit)
-        self.main_win()
-        self.add_links_win()
-        self.settings_win()
-        sys.exit(app.exec_())
+        # app.aboutToQuit.connect(self.actions.handle_exit)
+        # self.main_win()
+        # self.add_links_win()
+        # self.settings_win()
+        # sys.exit(app.exec_())
 
 
 Gui()
