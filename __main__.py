4f8185061fc0e38996e2af361166476c8b7cea59
Hosam <34396668+hosamn@users.noreply.github.com>
2023-12-16 00:02:42 +0200

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,1 +1,11 @@
-print("Hello world")
+from PyQt5.QtWidgets import QMessageBox
+
+
+# Alert
+def alert(text):
+    msg = QMessageBox()
+    msg.setIcon(QMessageBox.Warning)
+    msg.setWindowTitle('Alert')
+    msg.setText(text)
+
+alert("What in the Hell are you doing!")
